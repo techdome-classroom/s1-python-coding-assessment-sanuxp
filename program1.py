9a54a84f854959a527f3860b5b50de53f80fe1fc
Sanu Kumar <sanu848114@gmail.com>
2024-09-27 12:12:55 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -3,4 +3,7 @@ class Solution:
     def getTotalIsles(self, grid: list[list[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        
+        def dfs(row, column):
+            if row<0 or row == m or column<0 or column == n or grid[row][column] == 'W':
+                return
+            
