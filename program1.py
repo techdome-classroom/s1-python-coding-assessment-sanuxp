c1f2e2f6e52078a3eea30c085892404729f88d2f
Sanu Kumar <sanu848114@gmail.com>
2024-09-27 12:21:04 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -16,7 +16,7 @@ class Solution:
             for column in range(n):
                 if grid[row][column]=='L':
                     dfs(row, column)
-                count+=1
+                    count+=1
         return count
 if __name__ == "__main__":
     s = Solution()
