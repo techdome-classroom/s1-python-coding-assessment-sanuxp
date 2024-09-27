f6b131b8b877b5414697aee133250e8a09b5deff
Sanu Kumar <sanu848114@gmail.com>
2024-09-27 12:37:13 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,7 +1,7 @@
 def decode_message( s: str, p: str) -> bool:
         len1, len2, mat, star = 0, 0, 0, -1
         while len1<len(s):
-                if len2<len(p) and p[len2]=='?' or p[len2] == s[len1]:
+                if len2<len(p) and (p[len2]=='?' or p[len2] == s[len1]):
                         len1+=1
                         len2+=1
                 elif len2<len(p) and p[len2]=='*':
